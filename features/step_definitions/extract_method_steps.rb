052ae0ef3dca2c9a04c5393db17fce75e36ae0bb
Julian Churchill <chewielouie03@gmail.com>
2012-07-08 20:27:09 +0100

    Added .vimrc and corrected multi-line command arguments to remove white space and group correctly

--- features/step_definitions/extract_method_steps.rb
+++ features/step_definitions/extract_method_steps.rb
@@ -1,40 +1,36 @@
 require 'robot-vim'
 
 Given /^a ruby source buffer in vim with the following content:$/ do |buffer_content|
-  @vim_robot = RobotVim::Runner.new
+  @vim_robot = RobotVim::Runner.new( :vimrc => "features/support/.vimrc" )
   @buffer_content = buffer_content
 end
 
 def run_vim_commands commands
   response = @vim_robot.run( :input_file => @buffer_content, :commands => commands )
-  @buffer_contebt = response.body
+  @buffer_content = response.body
 end
 
 When /^I visually highlight "(.*?)"$/ do |text_to_highlight|
   commands = <<-COMMANDS
-    /#{text_to_highlight}
-    v#{text_to_highlight.size}l
-  COMMANDS
+/#{text_to_highlight}
+v#{text_to_highlight.size}l
+COMMANDS
   run_vim_commands commands
 end
 
-When /^I call the vim function ":ExtractMethod"$/ do
-  commands = <<-COMMANDS
-    :ExtractMethod
-  COMMANDS
-  run_vim_commands commands
+When /^I call the vim function "(.*?)"$/ do |function|
+  @vim_function_to_call = "ExtractMethod"
 end
 
 When /^I supply the argument "(.*?)"$/ do |argument|
-  # This hangs, probably because we don't prompt for an argument for :ExtractMethod yet...
-  #commands = <<-COMMANDS
-    ##{argument}
-  #COMMANDS
-  #run_vim_commands commands
+  commands = <<-COMMANDS
+:call #{@vim_function_to_call}()
+#{argument}
+COMMANDS
+  run_vim_commands commands
 end
 
 Then /^the buffer should contain:$/ do |expected_content|
   @buffer_content.should == expected_content
 end
-
 
