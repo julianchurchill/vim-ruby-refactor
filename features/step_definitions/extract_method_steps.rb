58c1a99f2258f751ad282b6119ab67e10d89b989
Julian Churchill <chewielouie03@gmail.com>
2012-07-08 12:32:59 +0100

    Filled in vim function step

--- features/step_definitions/extract_method_steps.rb
+++ features/step_definitions/extract_method_steps.rb
@@ -5,24 +5,35 @@ Given /^a ruby source buffer in vim with the following content:$/ do |buffer_con
   @buffer_content = buffer_content
 end
 
+def run_vim_commands commands
+  response = @vim_robot.run( :input_file => @buffer_content, :commands => commands )
+  @buffer_contebt = response.body
+end
+
 When /^I visually highlight "(.*?)"$/ do |text_to_highlight|
   commands = <<-COMMANDS
     /#{text_to_highlight}
     v#{text_to_highlight.size}l
   COMMANDS
-  @vim_robot.run( :input_file => @buffer_content, :commands => commands )
+  run_vim_commands commands
 end
 
 When /^I call the vim function ":ExtractMethod"$/ do
-  pending
+  commands = <<-COMMANDS
+    :ExtractMethod
+  COMMANDS
+  run_vim_commands commands
 end
 
-When /^I supply the argument "test_equality"$/ do
-  pending
+When /^I supply the argument "(.*?)"$/ do |argument|
+  #commands = <<-COMMANDS
+    ##{argument}
+  #COMMANDS
+  #run_vim_commands commands
 end
 
-Then /^the buffer should contain:$/ do
-  pending
+Then /^the buffer should contain:$/ do |expected_content|
+  @buffer_content.should == expected_content
 end
 
 
