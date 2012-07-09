20f00a0fc91566c1bebb310a52fd866ec7afcc74
Julian Churchill <chewielouie03@gmail.com>
2012-07-09 19:58:06 +0100

    Added initial tests and .rspec file

---
+++ spec/rubyrefactorer_spec.rb
@@ -0,0 +1,8 @@
+
+describe "RubyRefactorer" do
+  it "should cut the highlighted text from the vim buffer"
+  it "should paste the highlighted text after the current function"
+  it "should precede the pasted text with a new line and the function definition"
+  it "should include the function arguments in the definition"
+  it "should postfix the end keyword with an additional newline after the new function"
+end
