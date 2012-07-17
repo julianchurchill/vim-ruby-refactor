a87d01e73c3f0f1ca9d0109bfbf1835dfbffc4dd
Julian Churchill <chewielouie03@gmail.com>
2012-07-17 22:57:41 +0100

    Added unary conditional parsing and ignoring of identifiers starting with digits

--- lib/variableextractor.rb
+++ lib/variableextractor.rb
@@ -1,5 +1,12 @@
 
 class VariableExtractor
+  KEYWORDS = [
+    " and ",
+    " or ",
+    "&&",
+    "||"
+  ]
+
   def initialize text
     @text = text
   end
@@ -7,7 +14,32 @@ class VariableExtractor
   def extract
     return [ "a", "b" ] if @text == [ "a == b" ]
     return [ "a", "b" ] if @text == [ "a ==", "b" ]
-    []
+    return find_variables
+  end
+
+  def find_variables
+    text_without_keywords = @text
+    KEYWORDS.each { |k| text_without_keywords = remove_keyword text_without_keywords, k }
+    identifiers_only = remove_invalid_identifiers text_without_keywords
+    return identifiers_only[0].split " "
+  end
+
+  def remove_keyword text, keyword
+    parsed_text = []
+    escaped_keyword = escape_for_regex keyword
+    text.each { |t| parsed_text += [ t.gsub( /#{escaped_keyword}/, ' ' ) ] }
+    return parsed_text
+  end
+
+  def escape_for_regex text
+    return '\|\|' if text == "||"
+    text
+  end
+
+  def remove_invalid_identifiers text
+    parsed_text = []
+    text.each { |t| parsed_text += [ t.gsub( /\d+\w+/, '' ) ] }
+    return parsed_text
   end
 end
 
--- spec/variableextractor_spec.rb
+++ spec/variableextractor_spec.rb
@@ -10,4 +10,24 @@ describe "VariableExtractor" do
     v = VariableExtractor.new [ "a ==", "b" ]
     v.extract.should == [ "a", "b" ]
   end
+
+  it "should be able to extract variables from a unary conditionals" do
+    v = VariableExtractor.new [ "a and b and c or d && e" ]
+    v.extract.should == [ "a", "b", "c", "d", "e" ]
+  end
+
+  it "should be able to extract variables from unary conditional without spaces" do
+    v = VariableExtractor.new [ "a||b&&c" ]
+    v.extract.should == [ "a", "b", "c" ]
+  end
+
+  it "should be able to extract variables from an or conditional" do
+    v = VariableExtractor.new [ "a || b" ]
+    v.extract.should == [ "a", "b" ]
+  end
+
+  it "should ignore identifiers beginning with numbers" do
+    v = VariableExtractor.new [ "1banana 2apple a" ]
+    v.extract.should == [ "a" ]
+  end
 end
